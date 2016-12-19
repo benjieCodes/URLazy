c275818d1f387e18f67ff9bd978438830472d02c
benjiecodes <benjiecodes@gmail.com>
2016-12-19 13:18:31 -0500

    added logic to return a 404 if no alias is found

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 
+// this redirects us to different websites
 var mappings = {
     g: 'http://www.google.com',
     b: 'http://benjie.tech'
@@ -10,10 +11,15 @@ var mappings = {
 var server = http.createServer(function (req, res) {
     var alias = req.url.substring(1);
 
+    // returns a 404 not found if no alias is found
+    if (!mappings[alias]) {
+        res.writeHead(404)
+        return res.end()
+    }
     res.writeHead(302, {
         location: mappings[alias]
     });
     res.end();
 });
 
-server.listen(3000);
+server.listen(3000); //sets this to localhost:
