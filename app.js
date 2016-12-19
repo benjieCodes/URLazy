1542ad9a1a5036425dfef47eb90d30a03087a149
benjiecodes <benjiecodes@gmail.com>
2016-12-19 13:53:33 -0500

    added an API in module to return undefined for incorrect url

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ var http = require('http');
 var mappings = require('./mappings')
 
 var server = http.createServer(function (req, res) {
-    var alias = req.url.substring(1);
+    var mapping = mappings.get(req.url)
     var verb = req.method;
 
-    if (!mappings[alias]) {
-        res.writeHead(404)
-        res.write('Sorry the alias: ' + alias + ' was not found.' + ' (' + verb + ' REQUEST)')
+    if (!mapping) {
+        res.writeHead(404);
+        res.write('Sorry the alias was not found.' + ' (' + verb + ' REQUEST)')
         return res.end()
     }
 
     // returns user to website from the mappings
-    res.writeHead(302, {
-        location: mappings[alias]
-    });
+    res.writeHead(302, {location: mapping });
     res.end();
 });
 
