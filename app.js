0c9acb9919828025383ac79d350d1c2c11a52e8f
benjiecodes <benjiecodes@gmail.com>
2016-12-19 11:59:44 -0500

    added method

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-    res.write('Hello World');
+    var verb = req.method,
+        url = req.url;
+
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    })
+    res.write('Hello World' + verb + ' ' + url);
     res.end();
 });
 
